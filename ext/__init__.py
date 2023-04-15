6961e883c695e5839a52cedcef47e6ea26070ce1
elias <103325366+0x-65@users.noreply.github.com>
2023-04-15 13:12:23 +0300

    Delete ext directory

--- ext/__init__.py
+++
@@ -1,6 +0,0 @@
-from pkgutil import iter_modules
-
-#iterates through the current directory's files
-EXTENSIONS: list = [
-    module.name for module in iter_modules(__path__, f'{__package__}.')
-    ]
